d89556d32450d1efa31cf17f0be66191960a7af8
Mason <51417315+lookitsmason@users.noreply.github.com>
2022-05-23 11:25:06 -0400

    new verb method, versions added

---
+++ aaas-v2.py
@@ -0,0 +1,8 @@
+# AaaS - Adages as a Service v2.0
+
+# Project Goals
+# Display a start message/greeting.
+# Add two verbs to a set of phrases
+# Combine to create a new adage.
+
+# Example: Dont <insert verb> where you <insert verb>.
